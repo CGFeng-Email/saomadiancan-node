94f5aa253b328626fea811e7618d7cdb1a30457c
cgf <1291933022@qq.com>
2023-06-15 18:41:36 +0800

    mac更新

--- router/code/code.js
+++ router/code/code.js
@@ -38,7 +38,8 @@ const {
 	bufferImgName,
 } = require('../../jwt/buffer');
 
-// 生成小程序码
+
+// 生成小程序码 等到发布小程序之后 可以在后台自己生成二维码
 router.post('/produceCode', new verifyToken().m, async ctx => {
 	const {
 		table_number
@@ -51,17 +52,17 @@ router.post('/produceCode', new verifyToken().m, async ctx => {
 
 		// 查询当前桌号是否已经存在
 		const tableNumberListApi = `db.collection('tableNumberList').where({table_number: '${table_number}'}).get()`;
-		
+
 		const getTablenumber = await new getToken().publicApi(queryApi, tableNumberListApi)
 		console.log('getTablenumber', getTablenumber);
-		
+
 		if (getTablenumber.data.length > 0) {
 			new result(ctx, '该桌号已添加', 202).answer();
 		} else {
 
 			// 返回二维码二进制流
 			const res = await new getToken().produceCode(table_number);
-			
+
 			// 随机命名
 			const imgName = await bufferImgName(table_number);
 			console.log(imgName);
@@ -119,6 +120,95 @@ router.get('/getTableNumberList', new verifyToken().m, async ctx => {
 		new result(ctx, 'SUCCESS', 200, data).answer()
 	} catch (error) {
 		new result(ctx, '服务器发生错误', 500).answer()
+	}
+})
+
+// 柱状图 7天销售额
+router.get('/sales', new verifyToken().m, async ctx => {
+	// 最终得到的数据类型 [{time: '2023-6-15', account: 146}, {time: '2023-6-14', account: 178}]
+	try {
+		// 1~7天的数组
+		const dateList = [1, 2, 3, 4, 5, 6, 7];
+		// 返回1～7天的日期
+		const dataList = dateList.map(item => {
+			// 利用moment插件返回当前参数的上一天日期
+			return moment().utcOffset(8).subtract(item, 'days').format('YYYY-MM-DD')
+		})
+		console.log('dataList', dataList);
+		const strinDataList = JSON.stringify(dataList);
+		console.log('strinDataList', strinDataList);
+		// 把dataList数组里面的每一项作为参数，发起请求，有就返回参数的数据
+		const query = `db.collection('saleTimeList').where({time: db.command.in(${strinDataList})}).orderBy('time', 'asc').get()`;
+		const res = await new getToken().publicApi(queryApi, query);
+		console.log('res', res);
+		// 转化parse数据
+		const dataParse = res.data.map(item => {
+			return {
+				time: JSON.parse(item).time,
+				total_account: JSON.parse(item).total_account,
+				unix: moment(JSON.parse(item).time).unix() // 时间戳，后面的柱状图会根据时间戳进行排序
+			}
+		})
+		console.log('dataParse', dataParse);
+
+		// 生成1～7天的数组
+		const list_data = dataList.map(item => {
+			return {
+				time: item,
+				total_account: 0,
+				unix: moment(item).unix() // 时间戳，后面的柱状图会根据时间戳进行排序
+			}
+		})
+		console.log('list_data', list_data);
+		// 合并两个数组, 方便利用重复数据替换重复的数据，这样就可生成一个1～7天的数据数组
+		const obj = {};
+		const list = [...dataParse, ...list_data].reduce((prev, item) => {
+			// prev: 初始值，第一次遍历接受上一次的值[]。可以存储上一次遍历的结果，会一直存储到结束
+			// []: 决定返回的数据格式，{}：返回对象
+
+			console.log('prev', prev);
+			// prev[]
+			// prev undefined
+			// prev undefined
+			// prev undefined
+			// prev undefined
+			// prev undefined
+			// prev undefined
+			// prev undefined
+
+			// prev[]
+			// prev['2023-06-12']
+			// prev['2023-06-12', '2023-06-14']
+			// prev['2023-06-12', '2023-06-14', '2023-06-13']
+			// prev['2023-06-12', '2023-06-14', '2023-06-13', '2023-06-12']
+
+			console.log('item', item);
+			// item { time: '2023-06-12', total_account: 256, unix: 1686499200 }
+			// item { time: '2023-06-14', total_account: 0, unix: 1686672000 }
+			// item { time: '2023-06-13', total_account: 0, unix: 1686585600 }
+			// item { time: '2023-06-12', total_account: 0, unix: 1686499200 }
+			// item { time: '2023-06-11', total_account: 0, unix: 1686412800 }
+			// item { time: '2023-06-10', total_account: 0, unix: 1686326400 }
+			// item { time: '2023-06-09', total_account: 0, unix: 1686240000 }
+			// item { time: '2023-06-08', total_account: 0, unix: 1686153600 }
+
+			if(!obj[item.time]) {
+				prev.push(item)
+				obj[item.time] = true
+			}
+			return prev
+		}, [])
+		console.log('list', list);
+
+		// 根据unix时间戳排序
+		const sort_list = list.sort((A,B) => {
+			return (A.unix - B.unix) // - 降序，+升序
+		})
+
+		console.log('sort_list', sort_list);
+		new result(ctx, 'SUCCESS', 200, sort_list).answer();
+	} catch (e) {
+		new result(ctx, '服务器发生错误', 500).answer();
 	}
 })
 
